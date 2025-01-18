7a1b11e6688811174df440b8f5ed9ed693ba97df
Krishnaprasanth <dhawansaran14346@gmail.com>
2025-01-18 19:27:05 +0530

    feat(TDD-001): Handle empty and invalid inputs

--- src/main.py
+++ src/main.py
@@ -1,2 +1,12 @@
 def add(num_strs):
+  # Provided input is not of type string.
+  if not isinstance(num_strs, str):
+    return 0
+
+  # Handle both empty and undefined (None in Python) values
+  if not num_strs:
+    return 0
+
+  # Handle two numbers in a comma-separated string
+  # To be implemented
   pass
--- tests/index.py
+++ tests/index.py
@@ -1,9 +1,21 @@
 import unittest
+from src.main import add
 
-class TestSampleSetup(unittest.TestCase):
-    def test_execute_successfully(self):
-        """Test to check if 1 equals 1"""
-        self.assertEqual(1, 1)
+class TestUnexceptedInput(unittest.TestCase):
+    def test_empty_input(self):
+        """Should return zero when the input is empty or is not defined"""
+        self.assertEqual(add(""), 0)
+        self.assertEqual(add(None), 0)
+
+    def test_non_string_input(self):
+        """Should return zero when the input is not a string"""
+        self.assertEqual(add(100), 0)
+        self.assertEqual(add(True), 0)
+        self.assertEqual(add({}), 0)
+
+# To be handled
+class TestExpectedInput(unittest.TestCase):
+    pass
 
 if __name__ == "__main__":
     unittest.main()
